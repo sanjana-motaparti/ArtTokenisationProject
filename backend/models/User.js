1ab3af496847e8e79da339e7683c537284e0e290
tupesanjana <162443395+tupesanjana@users.noreply.github.com>
2025-11-12 21:02:37 +0530

    Add files via upload

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    desoPublicKey: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
